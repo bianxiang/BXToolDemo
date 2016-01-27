51276fcba3558615614af49448f56c0471abc72e
bianxiang <bian15216629448@163.com>
2016-01-27 10:37:19 +0800

    增加unicode编码打印出来是中文的功能，增加封装MBProgress的工具，增加UITextview的placeholder的类别

--- demo/Libs/MBProgressHUD/MBProgressHUD.h
+++ demo/Libs/MBProgressHUD/MBProgressHUD.h
@@ -1,12 +1,12 @@
 //
 //  MBProgressHUD.h
-//  Version 0.9.1
+//  Version 0.7
 //  Created by Matej Bukovinski on 2.4.09.
 //
 
 // This code is distributed under the terms and conditions of the MIT license. 
 
-// Copyright (c) 2009-2015 Matej Bukovinski
+// Copyright (c) 2013 Matej Bukovinski
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -33,7 +33,7 @@
 @protocol MBProgressHUDDelegate;
 
 
-typedef NS_ENUM(NSInteger, MBProgressHUDMode) {
+typedef enum {
 	/** Progress is shown using an UIActivityIndicatorView. This is the default. */
 	MBProgressHUDModeIndeterminate,
 	/** Progress is shown using a round, pie-chart like, progress view. */
@@ -46,16 +46,16 @@ typedef NS_ENUM(NSInteger, MBProgressHUDMode) {
 	MBProgressHUDModeCustomView,
 	/** Shows only labels */
 	MBProgressHUDModeText
-};
+} MBProgressHUDMode;
 
-typedef NS_ENUM(NSInteger, MBProgressHUDAnimation) {
+typedef enum {
 	/** Opacity animation */
 	MBProgressHUDAnimationFade,
 	/** Opacity + scale animation */
 	MBProgressHUDAnimationZoom,
 	MBProgressHUDAnimationZoomOut = MBProgressHUDAnimationZoom,
 	MBProgressHUDAnimationZoomIn
-};
+} MBProgressHUDAnimation;
 
 
 #ifndef MB_INSTANCETYPE
@@ -98,23 +98,21 @@ typedef void (^MBProgressHUDCompletionBlock)();
  * drawn centered as a rounded semi-transparent view which resizes depending on the user specified content.
  *
  * This view supports four modes of operation:
- *  - MBProgressHUDModeIndeterminate - shows a UIActivityIndicatorView
- *  - MBProgressHUDModeDeterminate - shows a custom round progress indicator
- *  - MBProgressHUDModeAnnularDeterminate - shows a custom annular progress indicator
- *  - MBProgressHUDModeCustomView - shows an arbitrary, user specified view (see `customView`)
+ * - MBProgressHUDModeIndeterminate - shows a UIActivityIndicatorView
+ * - MBProgressHUDModeDeterminate - shows a custom round progress indicator
+ * - MBProgressHUDModeAnnularDeterminate - shows a custom annular progress indicator
+ * - MBProgressHUDModeCustomView - shows an arbitrary, user specified view (@see customView)
  *
  * All three modes can have optional labels assigned:
- *  - If the labelText property is set and non-empty then a label containing the provided content is placed below the
- *    indicator view.
- *  - If also the detailsLabelText property is set then another label is placed below the first label.
+ * - If the labelText property is set and non-empty then a label containing the provided content is placed below the
+ *   indicator view.
+ * - If also the detailsLabelText property is set then another label is placed below the first label.
  */
 @interface MBProgressHUD : UIView
 
 /**
  * Creates a new HUD, adds it to provided view and shows it. The counterpart to this method is hideHUDForView:animated:.
- *
- * @note This method sets `removeFromSuperViewOnHide`. The HUD will automatically be removed from the view hierarchy when hidden.
- *
+ * 
  * @param view The view that the HUD will be added to
  * @param animated If set to YES the HUD will appear using the current animationType. If set to NO the HUD will not use
  * animations while appearing.
@@ -128,12 +126,10 @@ typedef void (^MBProgressHUDCompletionBlock)();
 /**
  * Finds the top-most HUD subview and hides it. The counterpart to this method is showHUDAddedTo:animated:.
  *
- * @note This method sets `removeFromSuperViewOnHide`. The HUD will automatically be removed from the view hierarchy when hidden.
- *
  * @param view The view that is going to be searched for a HUD subview.
  * @param animated If set to YES the HUD will disappear using the current animationType. If set to NO the HUD will not use
  * animations while disappearing.
- * @return YES if a HUD was found and removed, NO otherwise.
+ * @return YES if a HUD was found and removed, NO otherwise. 
  *
  * @see showHUDAddedTo:animated:
  * @see animationType
@@ -142,8 +138,6 @@ typedef void (^MBProgressHUDCompletionBlock)();
 
 /**
  * Finds all the HUD subviews and hides them. 
- *
- * @note This method sets `removeFromSuperViewOnHide`. The HUDs will automatically be removed from the view hierarchy when hidden.
  *
  * @param view The view that is going to be searched for HUD subviews.
  * @param animated If set to YES the HUDs will disappear using the current animationType. If set to NO the HUDs will not use
@@ -350,12 +344,6 @@ typedef void (^MBProgressHUDCompletionBlock)();
  */
 @property (assign) float margin;
 
-/**
- * The corner radius for the HUD
- * Defaults to 10.0
- */
-@property (assign) float cornerRadius;
-
 /** 
  * Cover the HUD background view with a radial gradient. 
  */
@@ -400,26 +388,10 @@ typedef void (^MBProgressHUDCompletionBlock)();
  */
 @property (MB_STRONG) UIFont* labelFont;
 
-/**
- * Color to be used for the main label. Set this property if the default is not adequate.
- */
-@property (MB_STRONG) UIColor* labelColor;
-
-/**
- * Font to be used for the details label. Set this property if the default is not adequate.
+/** 
+ * Font to be used for the details label. Set this property if the default is not adequate. 
  */
 @property (MB_STRONG) UIFont* detailsLabelFont;
-
-/** 
- * Color to be used for the details label. Set this property if the default is not adequate.
- */
-@property (MB_STRONG) UIColor* detailsLabelColor;
-
-/**
- * The color of the activity indicator. Defaults to [UIColor whiteColor]
- * Does nothing on pre iOS 5.
- */
-@property (MB_STRONG) UIColor *activityIndicatorColor;
 
 /** 
  * The progress of the progress indicator, from 0.0 to 1.0. Defaults to 0.0. 
@@ -430,15 +402,6 @@ typedef void (^MBProgressHUDCompletionBlock)();
  * The minimum size of the HUD bezel. Defaults to CGSizeZero (no minimum size).
  */
 @property (assign) CGSize minSize;
-
-
-/**
- * The actual size of the HUD bezel.
- * You can use this to limit touch handling on the bezel aria only.
- * @see https://github.com/jdg/MBProgressHUD/pull/200
- */
-@property (atomic, assign, readonly) CGSize size;
-
 
 /**
  * Force the HUD dimensions to be equal if possible. 
